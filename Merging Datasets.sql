479a51fc8a08db8474c52abf0cf08709eaad6e2e
C Suraj <81463699+surajhari@users.noreply.github.com>
2024-01-12 19:16:18 +0530

    Update Merging Datasets.sql

--- Merging Datasets.sql
+++ Merging Datasets.sql
@@ -1,31 +1,31 @@
 -- Creating a table tripdata_2022 combining the datasets for the entire year 2022
 
-CREATE TABLE IF NOT EXISTS `tripdata_2022.tripdata_2022` AS (
-  SELECT * FROM `tripdata_2022.tripdata_202201`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202202`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202203`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202204`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202205`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202206`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202207`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202208`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202209`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202210`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202211`
-  UNION ALL
-  SELECT * FROM `tripdata_2022.tripdata_202212`
+create table if not exists `tripdata_2022.tripdata_2022` as (
+  select * from `tripdata_2022.tripdata_202201`
+  union all
+  select * from `tripdata_2022.tripdata_202202`
+  union all
+  select * from `tripdata_2022.tripdata_202203`
+  union all
+  select * from `tripdata_2022.tripdata_202204`
+  union all
+  select * from `tripdata_2022.tripdata_202205`
+  union all
+  select * from `tripdata_2022.tripdata_202206`
+  union all
+  select * from `tripdata_2022.tripdata_202207`
+  union all
+  select * from `tripdata_2022.tripdata_202208`
+  union all
+  select * from `tripdata_2022.tripdata_202209`
+  union all
+  select * from `tripdata_2022.tripdata_202210`
+  union all
+  select * from `tripdata_2022.tripdata_202211`
+  union all
+  select * from `tripdata_2022.tripdata_202212`
 );
 
 --Checking the number of rows in the combined table
 
-SELECT COUNT(*) FROM `tripdata_2022.tripdata_2022`;
+select count(*) from `tripdata_2022.tripdata_2022`;
