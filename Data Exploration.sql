93ab141d4f3c230f05c68f6d24e776fe4661051d
C Suraj <81463699+surajhari@users.noreply.github.com>
2024-01-15 00:40:04 +0530

    Update Data Exploration.sql

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -6,19 +6,19 @@ where table_name = 'tripdata_2022';
 
 -- Checking the count of null values (if any) in each column
 
-select count(*) - count(ride_id) ride_id,
- count(*) - count(rideable_type) rideable_type,
- count(*) - count(started_at) started_at,
- count(*) - count(ended_at) ended_at,
- count(*) - count(start_station_name) start_station_name,
- count(*) - count(start_station_id) start_station_id,
- count(*) - count(end_station_name) end_station_name,
- count(*) - count(end_station_id) end_station_id,
- count(*) - count(start_lat) start_lat,
- count(*) - count(start_lng) start_lng,
- count(*) - count(end_lat) end_lat,
- count(*) - count(end_lng) end_lng,
- count(*) - count(member_casual) member_casual
+select count(*) - count(ride_id) as ride_id,
+ count(*) - count(rideable_type) as rideable_type,
+ count(*) - count(started_at) as started_at,
+ count(*) - count(ended_at) as ended_at,
+ count(*) - count(start_station_name) as start_station_name,
+ count(*) - count(start_station_id) as start_station_id,
+ count(*) - count(end_station_name) as end_station_name,
+ count(*) - count(end_station_id) as end_station_id,
+ count(*) - count(start_lat) as start_lat,
+ count(*) - count(start_lng) as start_lng,
+ count(*) - count(end_lat) as end_lat,
+ count(*) - count(end_lng) as end_lng,
+ count(*) - count(member_casual) as member_casual
 from `tripdata_2022.tripdata_2022`;
 
 -- Checking the count of duplicate values (if any)in the table
